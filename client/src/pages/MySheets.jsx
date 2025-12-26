d778908e619c439c01a40b1b5fceda0442b63875
Laroldz <113868048+Laroldz@users.noreply.github.com>
2025-12-26 10:18:30 -0600

    Update Cognito login URL for production

--- client/src/pages/MySheets.jsx
+++ client/src/pages/MySheets.jsx
@@ -75,7 +75,7 @@ function MySheets() {
           ) : (
             <div className="list">
               {/* TODO: Update your Cognito login URL for production */}
-              <a href="https://us-east-2fzo87xm4b.auth.us-east-2.amazoncognito.com/login?client_id=14k24a6kquof4pvr3iph8g7u5q&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fnotonmybingosheet.onrender.com%2Fcallback">
+              <a href="https://us-east-21winqvkkq.auth.us-east-2.amazoncognito.com/login?client_id=5a0k6f5kn0l6hvo04hm7sjec66&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fnotonmybingosheet.onrender.com%2Fcallback">
                 Login
               </a>
             </div>
