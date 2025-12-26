406af23050e55bdea121e7ba32e839aa84016251
Laroldz <113868048+Laroldz@users.noreply.github.com>
2025-12-26 10:15:30 -0600

    Update login URL for authentication

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
                 {ownerId ? (
                             <div className = "list">  <a href = "/logout">Logout</a></div>
                 ) : (
-                    <div className = "list">  <a href="https://us-east-24ih5nmtxg.auth.us-east-2.amazoncognito.com/login?client_id=51td6ev8t8vehrd553eaiti02c&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fnotonmybingosheet.onrender.com%2Fcallback/">Login</a></div>
+                    <div className = "list">  <a href="https://us-east-21winqvkkq.auth.us-east-2.amazoncognito.com/login?client_id=5a0k6f5kn0l6hvo04hm7sjec66&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fnotonmybingosheet.onrender.com%2Fcallback">Login</a></div>
                 )}
                 </div>
             </div>
